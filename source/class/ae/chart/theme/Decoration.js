d74f681c34bba995d5729a65b7cc7191ffc786dd
adeliz <adelise.skope@gmail.com>
2016-07-13 16:43:33 +0200

    Create qooxdoo application

---
+++ source/class/ae/chart/theme/Decoration.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("ae.chart.theme.Decoration",
+{
+  extend : qx.theme.modern.Decoration,
+
+  decorations :
+  {
+  }
+});
---
+++ source/class/ae/chart/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("ae.chart.theme.Theme",
+{
+  meta :
+  {
+    color : ae.chart.theme.Color,
+    decoration : ae.chart.theme.Decoration,
+    font : ae.chart.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : ae.chart.theme.Appearance
+  }
+});
