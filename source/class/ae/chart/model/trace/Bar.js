05d98b801c7194d663d6b02f1ac0a97a3df9cb82
adeliz <adelise.skope@gmail.com>
2016-09-09 15:56:49 +0200

    Created a base class for scatter and bar traces

--- source/class/ae/chart/model/trace/Bar.js
+++ source/class/ae/chart/model/trace/Bar.js
@@ -4,7 +4,7 @@
  * The data visualized by the span of the bars is set in `y` if `orientation` is set th *v* (the default) and the labels are set in `x`. By setting `orientation` to *h*, the roles are interchanged.
  */
 qx.Class.define("ae.chart.model.trace.Bar", {
-	extend : ae.chart.model.trace.Scatter,
+	extend : ae.chart.model.trace.BaseTrace,
 	
 	properties : {
 		
